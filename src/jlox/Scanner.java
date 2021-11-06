1bfbc11fd9640214d42dc2aa280f8da0d51d7af3
Tech0tron <tech0tron.roblox@gmail.com>
2021-11-06 15:27:21 -0600

    finish chapter 4 + .gitignore

--- src/jlox/Scanner.java
+++ src/jlox/Scanner.java
@@ -14,11 +14,33 @@ public class Scanner {
     private int current = 0;
     private int line = 1;
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+
     Scanner(String source) {
         this.source = source;
     }
 
-    List<Token> scanTokens() {
+    public List<Token> scanTokens() {
         while(!isAtEnd()) {
             // We are at the beginning of the next lexeme
             start = current;
@@ -77,7 +99,6 @@ public class Scanner {
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
-                Lox.error(line, "Unexpected character");
                 break;
         }
     }
@@ -118,7 +139,10 @@ public class Scanner {
     private void identifier() {
         while(isAlphaNumeric(peek())) advance();
 
-        addToken(IDENTIFIER);
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
     }
 
     // the output
