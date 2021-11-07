8c24c5ba62d5b2cd06b45be2d8ca682e18d6bb4c
Tech0tron <tech0tron.roblox@gmail.com>
2021-11-07 12:21:00 -0700

    parser chapter 6

--- src/jlox/Lox.java
+++ src/jlox/Lox.java
@@ -45,8 +45,18 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError) return;
+        System.out.println(new AstPrinter().print(expression));
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report (token.line, "at '" + token.lexeme + "'", message);
         }
     }
 
